b959cc486e032c55f2cd989a62757c35e4eaa5bf
unknown <jezdrav@gmail.com>
2023-04-21 19:23:24 +0200

    Commit changes on config.env and server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const { error } = require('console');
 
 dotenv.config({ path: './config.env' });
 
@@ -13,11 +14,48 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(() => console.log('DB connection succesful!'));
+
+////mongoose schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price.']
+  }
+});
+
+////mongose model
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'Wind from the fields ',
+  rating: 4.4,
+  price: 433
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR 😡', err);
+  });
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
